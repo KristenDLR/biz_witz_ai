6b9ed85f9bd7afed18cf64b9e771795b0a8a2193
Kristen De La Rosa <delarosa.kristen@gmail.com>
2025-08-31 02:14:05 -0700

    feat: add EDA module and update requirements

---
+++ modules/eda_report.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+def generate_profile_report(df, output_path="reports/eda_report.html"):
+    profile = ProfileReport(df, title="Sales Data EDA", explorative=True)
+    profile.to_file(output_path)
