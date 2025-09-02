b6851f3c242b6801b5b3abfd339d84af851214fd
Kristen De La Rosa <delarosa.kristen@gmail.com>
2025-09-01 22:49:47 -0700

    feat: build Langchain retreival pipeline

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from modules.data_loader import load_and_clean
 from modules.eda_report import generate_profile_report, summarize_eda
 from modules.eda_summary import save_summary_to_json
 from modules.vector_store import load_and_chunk_summary, build_vector_store, load_vector_store
+from modules.rag_chain import get_retriever, build_rag_chain
 import os
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
@@ -19,9 +20,17 @@ save_summary_to_json(summary)
 chunks = load_and_chunk_summary()
 vector_store = build_vector_store(chunks)
 
-
+# load vector store
 vector_store = load_vector_store()
 retriever = vector_store.as_retriever()
 docs = retriever.get_relevant_documents("What are the top-selling products?")
 for i, doc in enumerate(docs, 1):
     print(f"\nChunk {i}:\n{doc.page_content}")
+
+# RAG
+retriever = get_retriever(vector_store)
+rag_chain = build_rag_chain(retriever)
+
+# Remove later
+response = rag_chain.run("What are the top-selling products?")
+print("\nRAG Response:\n", response)
